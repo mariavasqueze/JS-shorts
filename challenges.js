fdcf434ce65082a0356b0614f6a9b6cfab73e393
Maria Vasquez <mariadelmarvze@gmail.com>
2021-10-25 18:02:24 -0500

    new short challenges

--- challenges.js
+++ challenges.js
@@ -31,7 +31,7 @@ for (let i = 0; i < aliens.length; i++){
 }
 
 // 3
-//Function that takes in an array as an argument and, using a loop, returns a new array with each string in the array prepended with 'baby '
+//Function that takes in an array as an argument and, using a loop, returns a new array with each string in the array starting with 'baby'
 const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
 
 const convertToBaby = animals => {
@@ -71,3 +71,62 @@ const shoutGreetings = arr => arr.map(word => word.toUpperCase() + '!');
     }
     return shoutArray
 }
+
+// 6 
+//Sort an array of numbers from lowest to highest
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+
+const sortYears = years.sort((a , b) => a - b); //highest to lowest would be 'b - a'
+console.log(sortYears);
+/* OR 
+const sortYears = arr => arr.sort((x, y) => y - x);
+console.log(sortYears(years))
+*/
+
+
+// 7 
+// return an array with the values that are present in BOTH given arrays
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway'];
+
+const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => secondArray.includes(item))
+
+/* OR
+// As a function declaration AND using named function w/ a loop:
+function justCoolStuff(firstArray, secondArray) {
+      function isInSecondArray(item){
+            for(let i = 0; i<secondArray.length; i++){
+                  if (secondArray[i] === item){
+                        return true
+                  }
+            }
+            return false 
+      }
+      return firstArray.filter(isInSecondArray)
+}
+*/
+
+// 8 
+// Function that takes an array wrote in object format and sort the array in ascending order based on the numbers
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+const sortSpeciesByTeeth = arr => arr.sort((speciesObj1, speciesObj2) => speciesObj1.numTeeth > speciesObj2.numTeeth)
+
+/*
+// As a function declaration AND using a named function:
+function sortSpeciesByTeeth(arr) {
+      const compareTeeth = (speciesObj1, speciesObj2) => speciesObj1.numTeeth > speciesObj2.numTeeth
+      return arr.sort(compareTeeth)
+}
+*/
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// 9 
+// Return the posotion of 'keys' if the string exists in the array
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+const findMyKeys = arr => arr.findIndex(item => item === 'keys')
+
+console.log(findMyKeys(randomStuff))
